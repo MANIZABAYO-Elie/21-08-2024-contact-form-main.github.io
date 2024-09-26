77259d4a163fcabbfe6507df84aa3ce1fd2dd668
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-26 10:32:10 +0200

    file committed

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const contact = document.getElementById('contact');
-const errorMessage = document.getElementById('error')
+const contactFm= document.getElementById('contact-fm');
+const errorMessage = document.getElementById('error');
+const successmessage = document.getElementById('success');
+contactFm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+const firstName = document.getElementById('first-name').value.trim();
+const lastName = document.getElementById('last-name').value.trim();
+const email  = document.getElementById('email').value.trim();
+const queryType = document.querySelector('input[name="query-type"]:checked');
+const message = documet.getElementById('message').value.trim();
+const contest = document.getElementById('contest').checked;
+const formAlert = document.querySelectorAll('.form-alert');
+
+})
