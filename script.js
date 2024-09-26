6ff3fd8a9a5c1312172b8b80e5058cd27e09ae44
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-26 11:00:37 +0200

    file committed

--- script.js
+++ script.js
@@ -10,5 +10,39 @@ const queryType = document.querySelector('input[name="query-type"]:checked');
 const message = documet.getElementById('message').value.trim();
 const contest = document.getElementById('contest').checked;
 const formAlert = document.querySelectorAll('.form-alert');
+// first name validation
+let isValid = true;
+if (firstName===""){
+    isValid = false;
 
+    document.querySelector('#first-name + .form-alert').style.display = "block";
+    document.querySelector('#first-name').style.border = " 1px solid red";
+}else{
+    document.querySelector('#first-name + .form-alert').style.display = "none";
+    document.querySelector('#first-name').style.border = " 1px solid gray";
+}
+// last name validation
+
+
+if (lastNametName===""){
+    isValid = false;
+
+    document.querySelector('#last-name + .form-alert').style.display = "block";
+    document.querySelector('#last-name').style.border = " 1px solid red";
+}else{
+    document.querySelector('#last-name + .form-alert').style.display = "none";
+    document.querySelector('#last-name').style.border = " 1px solid gray";
+
+}
+
+// Email validation
+if(!isValidEmail(email)){
+ isValid = false
+ document.querySelector('#email + .form-alert').style.display = "block";
+ document.querySelector('#email').style.border = " 1px solid red";
+}else{
+    document.querySelector('#email + .form-alert').style.display = "none";
+    document.querySelector('#email').style.border = " 1px solid gray";
+
+}
 })
